c53ae090abc860338bf163553a563a7313a6701b
Justin Riley <justin.t.riley@gmail.com>
2017-03-06 11:28:10 -0500

    anatomy: implement channel_metadata endpoint

--- ariadne_nda/api/anatomy/channel_metadata.py
+++ ariadne_nda/api/anatomy/channel_metadata.py
@@ -1,2 +1,13 @@
-def search():
-    return {'message': 'channel_metadata'}, 200
+from ariadne_nda import butterfly
+
+
+def search(experiment, sample, dataset, channel):
+    return butterfly.proxy(
+        '/api/channel_metadata',
+        params=dict(
+            experiment=experiment,
+            sample=sample,
+            dataset=dataset,
+            channel=channel,
+        )
+    )
