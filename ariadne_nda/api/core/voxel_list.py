b14595124d41e2b60d08fc39439772c159cbeaa1
Justin Riley <justin.t.riley@gmail.com>
2017-04-04 15:30:08 -0400

    S9: return empty result for now
    ...not implemented in butterfly yet

--- ariadne_nda/api/core/voxel_list.py
+++ ariadne_nda/api/core/voxel_list.py
@@ -1,2 +1,9 @@
-def get(*args, **kwargs):
-    pass
+import json
+from ariadne_nda import butterfly
+
+
+def get(collection, experiment, channel, resolution, xstart, xstop, ystart,
+        ystop, zstart, zstop, id):
+
+    # not implemented yet
+    return dict(x=[], y=[], z=[])
