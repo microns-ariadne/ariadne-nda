166c9ebaf721d4b5e51c0c15dcf02004eba4c157
Justin Riley <justin.t.riley@gmail.com>
2017-04-04 15:07:19 -0400

    implement S3

--- ariadne_nda/api/core/synapse_keypoint.py
+++ ariadne_nda/api/core/synapse_keypoint.py
@@ -1,2 +1,17 @@
-def get(*args, **kwargs):
-    pass
+import json
+from ariadne_nda import butterfly
+
+
+def get(collection, experiment, channel, resolution, id):
+    ret = butterfly.proxy(
+        '/api/entity_feature',
+        params=dict(
+            experiment=collection,
+            sample=collection,
+            dataset=experiment,
+            channel=channel,
+            feature='synapse_keypoint',
+            id=id,
+        )
+    )
+    return dict(keypoint=list(json.loads(ret.data).values()))
