37a0a6cab300cab7bd31e670bb745409ec7593c7
Justin Riley <justin.t.riley@gmail.com>
2017-04-04 15:13:43 -0400

    implement S4

--- ariadne_nda/api/core/synapse_parent.py
+++ ariadne_nda/api/core/synapse_parent.py
@@ -1,2 +1,24 @@
-def get(*args, **kwargs):
-    pass
+import json
+from ariadne_nda import butterfly
+
+
+def get(collection, experiment, channel, id):
+    ret = butterfly.proxy(
+        '/api/entity_feature',
+        params=dict(
+            experiment=collection,
+            sample=collection,
+            dataset=experiment,
+            channel=channel,
+            feature='synapse_parent',
+            id=id,
+        )
+    )
+    bfly_json = json.loads(ret.data)
+    parents = {}
+    for parent in bfly_json:
+        if parent.endswith('_pre'):
+            parents[bfly_json[parent]] = 1
+        if parent.endswith('_post'):
+            parents[bfly_json[parent]] = 2
+    return dict(parent_neurons=parents)
