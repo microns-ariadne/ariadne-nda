b5f84047035f0e34e34c5a6f0d27b9af9eacd2bf
Justin Riley <justin.t.riley@gmail.com>
2017-04-04 15:19:16 -0400

    implement S6

--- ariadne_nda/api/core/neuron_ids.py
+++ ariadne_nda/api/core/neuron_ids.py
@@ -1,2 +1,38 @@
-def get(*args, **kwargs):
-    pass
+import json
+from ariadne_nda import butterfly
+
+
+def get(collection, experiment, channel, resolution, xstart, xstop, ystart,
+        ystop, zstart, zstop):
+
+    # Scale all by the resolution
+    scale = 2 ** resolution
+
+    # Integer division for <x> and <y> origin
+    bfly_y = ystart // scale
+    bfly_x = xstart // scale
+    bfly_z = zstart
+
+    depth = zstop - zstart
+
+    # Integer division of differences for <width> and <height>
+    bfly_height = ( ystop - ystart ) // scale
+    bfly_width = ( xstop - xstart ) // scale
+
+    ret = butterfly.proxy(
+        '/api/entity_feature',
+        params=dict(
+            experiment=collection,
+            sample=collection,
+            dataset=experiment,
+            channel=channel,
+            feature='neuron_ids',
+            x=bfly_x,
+            y=bfly_y,
+            z=bfly_z,
+            width=bfly_width,
+            height=bfly_height,
+            depth=depth,
+        )
+    )
+    return dict(ids=json.loads(ret.data))
