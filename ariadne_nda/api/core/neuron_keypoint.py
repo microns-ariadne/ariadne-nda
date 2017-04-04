fec888b6e22102a73b26a12d5b38c099d0607363
Justin Riley <justin.t.riley@gmail.com>
2017-04-04 15:23:25 -0400

    implement S7

--- ariadne_nda/api/core/neuron_keypoint.py
+++ ariadne_nda/api/core/neuron_keypoint.py
@@ -1,2 +1,18 @@
-def get(*args, **kwargs):
-    pass
+import json
+from ariadne_nda import butterfly
+
+
+def get(collection, experiment, channel, resolution, id):
+    ret = butterfly.proxy(
+        '/api/entity_feature',
+        params=dict(
+            experiment=collection,
+            sample=collection,
+            dataset=experiment,
+            channel=channel,
+            feature='neuron_keypoint',
+            id=id,
+        )
+    )
+    bfly_json = json.loads(ret.data)
+    return dict(keypoint=[bfly_json['x'], bfly_json['y'], bfly_json['z']])
