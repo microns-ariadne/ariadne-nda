2d23c9fd1023a2e2b9b647fbbe2ad84d638d80b3
Justin Riley <justin.t.riley@gmail.com>
2017-05-23 14:11:53 -0400

    use projection to trim result when fetching cells

--- ariadne_nda/api/physiology/cells.py
+++ ariadne_nda/api/physiology/cells.py
@@ -4,5 +4,5 @@ from ariadne_npdb.models import cell
 def get(run_id):
     c = cell.Cell()
     q = {'run': run_id}
-    res = [i['id'] for i in c.find(q)]
+    res = [i['id'] for i in c.find(q, projection=['id'])]
     return res or []
